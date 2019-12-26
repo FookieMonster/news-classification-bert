e15c968fa164f930e026039ea8df26dee6de1e6b
FookieMonster <fuukiemonster@gmail.com>
2019-12-26 16:51:27 +0900

    endpoints I/F definition

--- classification-demo-server/classification-demo-endpoints/src/main/java/com/fookiemonsters/demo/endpoints/NewsClassificationEndpoint.java
+++ classification-demo-server/classification-demo-endpoints/src/main/java/com/fookiemonsters/demo/endpoints/NewsClassificationEndpoint.java
@@ -1,5 +1,27 @@
 package com.fookiemonsters.demo.endpoints;
 
+import com.fookiemonsters.demo.model.viewmodel.NewsClassificationRequest;
+import com.fookiemonsters.demo.model.viewmodel.NewsClassificationResponse;
+import com.fookiemonsters.demo.service.NewsClassificationService;
+import com.fookiemonsters.demo.service.exception.NewsClassificationServiceException;
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.inject.Inject;
+
+import java.io.IOException;
+
+import static com.google.api.server.spi.config.ApiMethod.HttpMethod.POST;
+
+@Api(name = "news_classification", version = "v1")
 public class NewsClassificationEndpoint {
 
+	@Inject
+	private NewsClassificationService service;
+
+	@ApiMethod(name = "classify", path = "classify", httpMethod = POST)
+	public NewsClassificationResponse classify(NewsClassificationRequest req)
+			throws IOException, NewsClassificationServiceException {
+
+		return service.classify(req.getTitles());
+	}
 }
