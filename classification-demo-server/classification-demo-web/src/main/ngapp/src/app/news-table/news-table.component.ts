917ac2ca3cf3feb66cd27a0ea6bc408366c9e8a9
FookieMonster <fuukiemonster@gmail.com>
2020-01-03 01:00:23 +0900

    ngapp

--- classification-demo-server/classification-demo-web/src/main/ngapp/src/app/news-table/news-table.component.ts
+++ classification-demo-server/classification-demo-web/src/main/ngapp/src/app/news-table/news-table.component.ts
@@ -5,7 +5,7 @@ import { NewsRssResult } from '../model/news-rss-result';
 import { NewsClassificationResult } from '../model/news-classification-result';
 import { NewsRssRequest } from '../model/news-rss-request';
 import { NewsClassificationRequest } from '../model/news-classification-request';
-import { SelectItem } from 'primeng/api';
+import { Message, SelectItem } from 'primeng/api';
 
 interface Description {
   name: string;
@@ -34,6 +34,7 @@ export class NewsTableComponent implements OnInit {
 
   descriptions: Description[];
   visibleProgressBar: boolean;
+  msgs: Message[] = [];
 
   constructor(private newsClassificationService: NewsClassificationService, private newsRssService: NewsRssService) {
   }
@@ -46,15 +47,15 @@ export class NewsTableComponent implements OnInit {
     ];
 
     this.newsSites = [
-      { label: '独女通信', value: { id: 0, label: 0, name: 'dokujo-tsushin', url: null } },
-      { label: 'ITライフハック', value: { id: 1, label: 1, name: 'it-life-hack', url: 'https://news.livedoor.com/rss/article/vender/ithack' } },
-      { label: '家電チャンネル', value: { id: 2, label: 2, name: 'kaden-channel', url: 'https://news.livedoor.com/rss/article/vender/kadench' } },
-      { label: 'livedoor HOMME', value: { id: 3, label: 3, name: 'livedoor-homme', url: null } },
-      { label: 'MOVIE ENTER', value: { id: 4, label: 4, name: 'movie-enter', url: 'https://news.livedoor.com/rss/article/vender/movie_enter' } },
-      { label: 'Peachy', value: { id: 5, label: 5, name: 'peachy', url: 'https://news.livedoor.com/rss/article/vender/saishoku_kenbi' } },
-      { label: 'エスマックス', value: { id: 6, label: 6, name: 'smax', url: 'https://news.livedoor.com/rss/article/vender/smax' } },
-      { label: 'Sports Watch', value: { id: 7, label: 7, name: 'sports-watch', url: 'https://news.livedoor.com/rss/article/vender/sports_watch' } },
-      { label: 'トピックニュース', value: { id: 8, label: 8, name: 'topic-news', url: 'https://news.livedoor.com/rss/article/vender/news' } }
+      { label: '独女通信', value: { id: 0, label: 0, url: null } },
+      { label: 'ITライフハック', value: { id: 1, label: 1, url: 'https://news.livedoor.com/rss/article/vender/ithack' } },
+      { label: '家電チャンネル', value: { id: 2, label: 2, url: 'https://news.livedoor.com/rss/article/vender/kadench' } },
+      { label: 'livedoor HOMME', value: { id: 3, label: 3, url: null } },
+      { label: 'MOVIE ENTER', value: { id: 4, label: 4, url: 'https://news.livedoor.com/rss/article/vender/movie_enter' } },
+      { label: 'Peachy', value: { id: 5, label: 5, url: 'https://news.livedoor.com/rss/article/vender/saishoku_kenbi' } },
+      { label: 'エスマックス', value: { id: 6, label: 6, url: 'https://news.livedoor.com/rss/article/vender/smax' } },
+      { label: 'Sports Watch', value: { id: 7, label: 7, url: 'https://news.livedoor.com/rss/article/vender/sports_watch' } },
+      { label: 'トピックニュース', value: { id: 8, label: 8, url: 'https://news.livedoor.com/rss/article/vender/news' } }
     ];
     this.newsSite = this.newsSites[6].value;
 
@@ -77,10 +78,19 @@ export class NewsTableComponent implements OnInit {
     const req = new NewsRssRequest();
     req.feedUrl = this.newsSite.url;
 
+    if (req.feedUrl == null) {
+      this.msgs = [{
+        severity: 'error',
+        summary: 'エラー',
+        detail: 'ニュースサイトは既に閉鎖されています。'
+      }];
+      this.visibleProgressBar = false;
+      return;
+    }
+
     this.newsRssService.feed(req)
       .subscribe(results => {
         this.feeds = results['results'];
-        this.visibleProgressBar = false;
         this.loadPredictions();
       });
   }
